519e1106e7999c48a98d77960ebb948fa4bc3062
seansemple <sean.semple@wsu.edu>
2025-10-29 20:26:19 -0700

    cleaned up js

--- dark-flower/script.js
+++ dark-flower/script.js
@@ -1,15 +1,10 @@
-// set up DOM loading event listener 
 document.addEventListener("DOMContentLoaded", function () {
 
-    // find the dark mode button
     let darkModeButton = document.querySelector("#toggleDarkMode");
 
-    // add a click function and handler
     darkModeButton.addEventListener("click", function (e) {
-        
-        // when clicked, add class to the body called "darkMode"
+
         document.body.classList.toggle("darkMode");
-        
     });
 
 });
